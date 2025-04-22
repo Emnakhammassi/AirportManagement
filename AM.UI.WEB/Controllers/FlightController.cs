16d953f796951d801e55f0457e445920ec1d97c7
EmnaKhammassi <emna.khammassi@esprit.tn>
2025-04-22 15:47:25 +0100

    Création du projet web

---
+++ AM.UI.WEB/Controllers/FlightController.cs
@@ -0,0 +1,96 @@
+﻿using AM.Applactioncore.Domaine;
+using AM.Applactioncore.Intterfaces;
+using AM.Applactioncore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AM.UI.WEB.Controllers
+{
+    public class FlightController : Controller
+    {
+        IServiceFlight sf;
+
+        public FlightController(IServiceFlight sf)
+        {
+            this.sf = sf;
+        }
+
+        // GET: FlightController
+        public ActionResult Index()
+        {   
+            return View(sf.GetMany());
+        }
+
+        // GET: FlightController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FlightController/Create
+        //formulair vide
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FlightController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Flight collection)
+        {
+            try
+            {
+                sf.Add(collection);
+                sf.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FlightController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FlightController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
